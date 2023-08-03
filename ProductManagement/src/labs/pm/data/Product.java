9f7f78c5794ad03bad728f2d03777756e655816e
IvanaCandyAguilar <ivanacandy1@gmail.com>
2023-08-03 22:51:50 +0200

    Practice 5 concluded, add method apply rating, defined new product and constructors

--- ProductManagement/src/labs/pm/data/Product.java
+++ ProductManagement/src/labs/pm/data/Product.java
@@ -29,35 +29,60 @@ public class Product {
     * Discount rate is 10%
     * */
     public static final BigDecimal DISCOUNT_RATE =  BigDecimal.valueOf(0.1);
+    private Rating rating;
+
+    public Product(){
+        this(0,"no name", BigDecimal.ZERO);
+    }
+
+    public Product(int ID, String name,  BigDecimal price, Rating rating) {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+
+    }
+
+    public Product(int ID, String name, BigDecimal price) {
+        this(ID, name, price, Rating.NOT_RATED);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
 
     public int getID() {
         return ID;
     }
 
-    public void setID(final int ID) {
+   /* public void setID(final int ID) {
         this.ID = ID;
-    }
+    }*/
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
+   /* public void setName(final String name) {
         this.name = name;
-    }
+    }*/
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
+    /*public void setPrice(final BigDecimal price) {
         //price = BigDecimal.ONE;
         this.price = price;
-    }
+    }*/
 
     private BigDecimal price;
 
     public BigDecimal getDiscount(){
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Product applyRating(Rating newRating){
+        return new Product(ID, name, price, newRating);
     }
 }
